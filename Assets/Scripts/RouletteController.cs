d94619636fb70efe189f02f97a05d34eee5b974f
kakke18 <tanaka-kakeru-xm@ynu.jp>
2018-06-21 23:44:12 +0900

    Fix RouletteController.cs

--- Assets/Scripts/RouletteController.cs
+++ Assets/Scripts/RouletteController.cs
@@ -3,12 +3,28 @@ using UnityEngine;
 
 public class RouletteController : MonoBehaviour {
 	float rotSpeed = 0;
+	bool flag = false;
+
+	private readonly string[] RESULT_NUM = new string[]{
+		"1", "2", "3", "4", "5",
+		"6", "7", "8", "9", "10"
+	};
+	public UnityEngine.UI.Text resultLabel;
 
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
 			this.rotSpeed = 10;
+			this.flag = true;
 		}
+
 		transform.Rotate(0, 0, this.rotSpeed);
-		this.rotSpeed *= 0.97f;
+		this.rotSpeed *= Random.Range(0.80f, 0.99f);
+		int num = Mathf.FloorToInt((transform.eulerAngles.z + 54) / 36);
+		if (num == 11) {
+			num = 1;
+		}
+		if (this.rotSpeed <= 0.05 && this.flag) {
+			resultLabel.text = num.ToString();
+		}
 	}
 }
