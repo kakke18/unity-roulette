f4fd11e3338f584eb78c025f0efc4779c25b04eb
kakke18 <tanaka-kakeru-xm@ynu.jp>
2018-06-21 20:18:42 +0900

    Fix Scripts file

--- Assets/Scripts/RouletteController.cs
+++ Assets/Scripts/RouletteController.cs
@@ -1,14 +1,14 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class RouretteController : MonoBehaviour {
+public class RouletteController : MonoBehaviour {
 	float rotSpeed = 0;
 
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
 			this.rotSpeed = 10;
 		}
-
 		transform.Rotate(0, 0, this.rotSpeed);
+		this.rotSpeed *= 0.97f;
 	}
 }
